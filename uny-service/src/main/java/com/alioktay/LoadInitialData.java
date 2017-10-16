ad396a87911ee02ce9abba3c48f67eba416326e2
Ali OKTAY <alioktay@gmail.com>
2017-10-16 18:05:17 +0300

    Randomized sample data

--- uny-service/src/main/java/com/alioktay/LoadInitialData.java
+++ uny-service/src/main/java/com/alioktay/LoadInitialData.java
@@ -11,6 +11,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Date;
 
 @Component
@@ -25,25 +26,31 @@ public class LoadInitialData implements CommandLineRunner {
         this.orderRepository = orderRepository;
     }
 
+
     @Override
     public void run(String... strings) throws Exception {
-        Customer customer = this.customerRepository.save(new Customer("John Doe", "Graz / Austria", new Date(), false, Type.MAJOR));
-        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "product 1", new BigDecimal(100.0d), Status.CONFIRMED, customer));
-        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "product 2", new BigDecimal(2000.0d), Status.DELIVERED, customer));
-        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "product 3", new BigDecimal(3000.0d), Status.IN_PROGRESS, customer));
-        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "product 4", new BigDecimal(5000.0d), Status.ORDERED, customer));
+        Customer customer = this.customerRepository.save(new Customer("Leanne Graham", "Kulas Light Apt. 556 Gwenborough", new Date(), false, Type.MAJOR));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Intelligent Fresh Mouse", new BigDecimal(529), Status.CONFIRMED, customer));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Generic Granite Cheese", new BigDecimal(691), Status.DELIVERED, customer));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Unbranded Concrete Pants", new BigDecimal(120), Status.IN_PROGRESS, customer));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Rustic Granite Ball", new BigDecimal(5000), Status.ORDERED, customer));
 
-        Customer customer2 = this.customerRepository.save(new Customer("Ali Oktay", "Istanbul / Turkey", new Date(), true, Type.MINOR));
-        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "product 1", new BigDecimal(100.0d), Status.CONFIRMED, customer2));
-        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "product 2", new BigDecimal(2000.0d), Status.DELIVERED, customer2));
-        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "product 3", new BigDecimal(6000.0d), Status.IN_PROGRESS, customer2));
-        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "product 4", new BigDecimal(22000.0d), Status.ORDERED, customer2));
+        Customer customer2 = this.customerRepository.save(new Customer("Ervin Howell", "Victor Plains Suite 879 Wisokyburgh", new Date(), true, Type.MINOR));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Awesome Frozen Pants", new BigDecimal(100), Status.CONFIRMED, customer2));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Licensed Rubber Car", new BigDecimal(2000), Status.DELIVERED, customer2));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Handcrafted Cotton Table", new BigDecimal(6000), Status.IN_PROGRESS, customer2));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Steel Towels", new BigDecimal(22000), Status.ORDERED, customer2));
 
-        Customer customer3 = this.customerRepository.save(new Customer("Nikola Tesla", "Belgrade / Serbia", new Date(), true, Type.VIP));
-        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "product 1", new BigDecimal(150.0d), Status.CONFIRMED, customer3));
-        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "product 2", new BigDecimal(3000.0d), Status.DELIVERED, customer3));
-        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "product 3", new BigDecimal(5100.0d), Status.IN_PROGRESS, customer3));
-        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "product 4", new BigDecimal(16000.0d), Status.ORDERED, customer3));
+        Customer customer3 = this.customerRepository.save(new Customer("Clementine Bauch", "Douglas Extension Suite 847 McKenziehaven", new Date(), true, Type.VIP));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Awesome Granite Mouse", new BigDecimal(150), Status.CONFIRMED, customer3));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Licensed Wooden Chair", new BigDecimal(3000), Status.DELIVERED, customer3));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Sleek Cotton Bacon", new BigDecimal(5100), Status.IN_PROGRESS, customer3));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Refined Fresh Table", new BigDecimal(16000), Status.ORDERED, customer3));
 
+        Customer customer4 = this.customerRepository.save(new Customer("Patricia Lebsack", "Hoeger Mall Apt. 692 South Elvis", new Date(), true, Type.POOR));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Handcrafted Plastic Sausages", new BigDecimal(150), Status.CONFIRMED, customer4));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Ergonomic Steel Cheese", new BigDecimal(3000), Status.DELIVERED, customer4));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Refined Soft Keyboard", new BigDecimal(5100), Status.IN_PROGRESS, customer4));
+        this.orderRepository.save(new Order(new Date(), new Date(), new Date(), "Gorgeous Wooden Pizza", new BigDecimal(16000), Status.ORDERED, customer4));
     }
 }
